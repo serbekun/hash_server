4e14f737f58c5fdf980a931846c93c9d358038e0
Mishchenko Sergey <sergey994049@gmail.com>
2025-11-09 13:17:12 +0900

    add first sire

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, jsonify
-import os
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -11,6 +10,10 @@ app.logger.disabled = True
 SERVER_SUDO_PASSWORD = "qwerty"
 USERS_DATA_SAVE_FOLDER_PATH = "users/"
 
+# sites files
+SITES_FOLDER = "sites/"
+MAIN_SITE = "main/"
+
 @app.route("/hello_world")
 def hello_world():
 
@@ -21,10 +24,17 @@ def hello_world():
 
 @app.route("/")
 def ok():
-    return f"ok http://{host}:{port}"
+    
+    print(f"{request.remote_addr} request /")
+    
+    html = ""
+    with open(SITES_FOLDER + MAIN_SITE + "index.html", "r") as f:
+        html = f.read()
+        
+    return f"{html}"
 
 if __name__ == "__main__":
-    host = "172.19.150.39"
+    host = "192.168.3.19"
     port = 2222
 
     print(f"server started on http://{host}:{port}")
