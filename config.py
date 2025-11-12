33364f1b4d630419a4df208005d947cef934c7fd
Mishchenko Sergey <sergey994049@gmail.com>
2025-11-12 23:10:56 +0900

    change variables to config

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 class Config:
+    """
+    Class for contain info about
+    - hosted
+    - port
+    - folder paths
+    - file paths
+    """
     HOST = "192.168.3.19"
     PORT = 2222
     SITES_FOLDER = "sites/"
     MAIN_SITE = "main/"
-    HASHING_PHOTO_SITE = "hashing_photo/"
+    HASHING_FILE_SITE = "hashing_file/"
     USERS_DATA_SAVE_FOLDER_PATH = "users/"
